8ad1dfbb186543504823d738ba797f9cc3cd6b38
tim <tim@glo.id.au>
2021-07-25 18:01:36 +1000

    adding an example trying to rewire SimpleText component
    Signed-off-by: tim <tim@glo.id.au>

--- src/components/Moo/Moo.component.tsx
+++ src/components/Moo/Moo.component.tsx
@@ -1,4 +1,5 @@
 import { useSimpleHook } from "./simple-number.hook";
+import { SimpleText } from "@imtbl/design-system/dist/minified";
 
 interface MooPropTypes {
   name: string;
@@ -13,8 +14,12 @@ export const demoString = "i wish i could be rewired ...";
 export function Moo({ name }: MooPropTypes) {
   const { number } = useSimpleHook();
   return (
-    <div>
-      {name} {demoFunction()} {number} {JSON.stringify(demoObject)} {demoString}
-    </div>
+    <>
+      <div>
+        {name} {demoFunction()} {number} {JSON.stringify(demoObject)}{" "}
+        {demoString}
+      </div>
+      <SimpleText>demo DS component</SimpleText>
+    </>
   );
 }
